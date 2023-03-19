5e86e800c20bf679d9f3d28e0c2515c7914f2398
tijsgroenendaal <tijsgroenendaal@debian.groenendaal>
2023-03-19 17:56:37 +0100

    add notice for observability

--- populator/populator.sql
+++ populator/populator.sql
@@ -24,13 +24,15 @@ $$
     declare
         r_group record;
         resource record;
+	resource_id integer;
     begin
         for r_group in 1..10 loop
             INSERT INTO resource_group
             VALUES (r_group);
             for resource in 1..10 loop
+		resource_id = (r_group * 1000) + resource;
                 INSERT INTO resources
-                VALUES ((r_group * 1000) + resource, r_group);
+                VALUES (resource_id, r_group);
 
                     INSERT INTO timeseries
                     SELECT
@@ -38,8 +40,9 @@ $$
                         -- Add 10 seconds to each series
                         ('2020-01-01 00:00:00.000'::timestamp) + ((serie * 10) * interval '1 second') as datetime,
                         random() * 100 > 2 as status,
-                        (r_group * 1000) + resource
+                        resource_id
                     FROM generate_series(1, 535680) as serie;
+		    raise notice 'inserted resource %', resource_id;
                 end loop;
             end loop;
     end;
